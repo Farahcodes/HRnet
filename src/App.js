78f9d6ab228916796c60c70654ea907847d090e7
Farah Fournat <farah.fournat@gmail.com>
2023-08-05 15:42:03 +0200

    added FormEmployee component to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import './App.scss';
+import { Suspense, lazy } from 'react';
+import { Link } from 'react-router-dom';
+const FormEmployee = lazy(() => import('./components/FormEmployee'));
 
 const App = () => {
-  return <div className="App"></div>;
+  return (
+    <main className="container">
+      <h1>HRnet</h1>
+      <Link to="employees" className="link">
+        View Current Employees
+      </Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <FormEmployee />
+      </Suspense>
+    </main>
+  );
 };
 
 export default App;
