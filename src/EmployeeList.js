b8b221f50e8e9b69839504da78ac5f4dfe83221c
Farah Fournat <farah.fournat@gmail.com>
2023-08-08 17:14:01 +0200

    EmployeeList complete

--- src/EmployeeList.js
+++ src/EmployeeList.js
@@ -1,12 +1,48 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { DataGrid } from '@mui/x-data-grid';
+import { useSelector } from 'react-redux';
+import './EmployeeList.scss';
 
 const EmployeeList = () => {
+  const [pageSize, setPageSize] = useState(5);
+  const employees = useSelector((state) => state.employees);
+  const columns = [
+    { headerName: 'First Name', field: 'firstName', minWidth: 50 },
+    { headerName: 'Last Name', field: 'lastName', minWidth: 50 },
+    { headerName: 'Start Date', field: 'startDate', minWidth: 150 },
+    { headerName: 'Department', field: 'department', minWidth: 50 },
+    { headerName: 'Street', field: 'street', minWidth: 200 },
+    { headerName: 'City', field: 'city', minWidth: 50 },
+    { headerName: 'State', field: 'state', minWidth: 50 },
+    { headerName: 'Zip Code', field: 'zipCode', minWidth: 50 },
+    {
+      headerName: 'Date of Birth',
+      field: 'dateOfBirth',
+      minWidth: 150,
+      flex: 1,
+    },
+  ];
+
   return (
-    <div id="employee-div" className="container">
+    <main className="container">
       <h1>Current Employees</h1>
-      <table id="employee-table" className="display"></table>
-      <Link to="/">Home</Link>
-    </div>
+      <Link to="/" className="link">
+        Home
+      </Link>
+      <div className="dataStyle">
+        <DataGrid
+          autoHeight
+          rows={employees}
+          columns={columns}
+          pageSize={pageSize}
+          onPageSizeChange={(newPage) => setPageSize(newPage)}
+          pagination
+          rowsPerPageOptions={[5, 10, 25, 50]}
+          disableSelectionOnClick
+        />
+      </div>
+    </main>
   );
 };
 
