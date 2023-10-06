c692e376053f680c7507baf09146ab7813d7a2a8
Farah Fournat <farah.fournat@gmail.com>
2023-10-06 15:05:37 +0200

    added documentation for the redux store

--- src/store.js
+++ src/store.js
@@ -12,6 +12,19 @@ import {
 } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
+/**
+ * `employeeSlice`
+ *
+ * This slice is responsible for managing the employee state within the Redux store.
+ * The slice contains an initial state and reducers to manage different state updates.
+ *
+ * `initialState`: An object that describes the initial state of the employee slice of the store.
+ *  - `employees`: An array meant to hold employee objects.
+ *
+ * `reducers`: An object containing Redux reducer functions.
+ *  - `add`: A reducer that takes the current state and an action, and adds a new employee object to the state.
+ */
+
 const employeeSlice = createSlice({
   name: 'employee',
   initialState: { employees: [] },
@@ -22,17 +35,40 @@ const employeeSlice = createSlice({
   },
 });
 
+/**
+ * `persistConfig`
+ *
+ * This configuration object is used to define how the redux state is persisted.
+ *
+ * `key`: The key for the persisted state in local storage.
+ * `version`: Version number of the reducer.
+ * `storage`: Define which storage engine to use.
+ */
 const persistConfig = {
   key: 'root',
   version: 1,
   storage,
 };
 
+/**
+ * `persistedReducer`
+ *
+ * Uses the `persistReducer` method from `redux-persist` to enhance the base reducer
+ * with persistence capabilities, wrapping the employeeSlice reducer with persist functionality.
+ */
 const persistedReducer = persistReducer(
   persistConfig,
   employeeSlice.reducer
 );
 
+/**
+ * `store`
+ *
+ * Uses `configureStore` from Redux Toolkit to create the application’s store
+ * `reducer`: The reducer function implemented by `persistedReducer`.
+ * `middleware`: Customizes the middleware using `getDefaultMiddleware` to ensure
+ *               the persistence actions do not get flagged in the serializable check middleware.
+ */
 const store = configureStore({
   reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
@@ -50,6 +86,12 @@ const store = configureStore({
     }),
 });
 
+/**
+ * `persistor`
+ *
+ * Uses `persistStore` to create a persistor object based on the configured store,
+ * which will be used to manage the store’s persistence lifecycle.
+ */
 const persistor = persistStore(store);
 
 export { store, persistor };
