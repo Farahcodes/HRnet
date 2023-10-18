b0b9a066d52b22bb830ab1904c881e9110175a27
Farah Fournat <farah.fournat@gmail.com>
2023-10-18 10:43:09 +0200

    added documentation for EmployeeList

--- src/EmployeeList.js
+++ src/EmployeeList.js
@@ -4,9 +4,25 @@ import { DataGrid } from '@mui/x-data-grid';
 import { useSelector } from 'react-redux';
 import './EmployeeList.scss';
 
+/**
+ *
+ * This component renders a list of current employees in a tabular format.
+ * The data for the employees is fetched from the Redux store.
+ * The table supports features like pagination and dynamic page size.
+ *
+ * Features:
+ * 1. Displays a list of employees with their detailed information.
+ * 2. Provides a link to navigate back to the Home page.
+ * 3. Utilizes the DataGrid component from the MUI library for tabular display.
+ * 4. Supports pagination and allows the user to choose the number of rows per page.
+ */
+
 const EmployeeList = () => {
+  // State for managing the number of rows displayed per page.
   const [pageSize, setPageSize] = useState(5);
+  // Fetch the list of employees from the Redux store.
   const employees = useSelector((state) => state.employees);
+  // Column configuration for the DataGrid component.
   const columns = [
     { headerName: 'First Name', field: 'firstName', minWidth: 50 },
     { headerName: 'Last Name', field: 'lastName', minWidth: 50 },
@@ -27,10 +43,12 @@ const EmployeeList = () => {
   return (
     <main className="container">
       <h1>Current Employees</h1>
+      {/* Navigation link to go back to the Home page */}
       <Link to="/" className="link">
         Home
       </Link>
       <div className="dataStyle">
+        {/* DataGrid component for displaying employees' information in a table format */}
         <DataGrid
           autoHeight
           rows={employees}
